6898d7e2bebaf0148789e1cb2a6cb55c2529d550
tyange <usun16@gmail.com>
2024-09-17 22:51:13 +0900

    chore: with supabase local

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  baseURL: "https://tmgztvdsoxoyhckhzeow.supabase.co/rest/v1/",
+  baseURL: `${import.meta.env.VITE_SUPABASE_URL}/rest/v1/`,
   headers: {
     apiKey: import.meta.env.VITE_SUPABASE_ANON_KEY,
     Authorization: `Bearer ${import.meta.env.VITE_SUPABASE_ANON_KEY}`,
