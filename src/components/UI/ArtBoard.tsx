ad0909a6b0425d495c7cb0a01529ef28c3642f4a
tyange <usun16@gmail.com>
2024-09-21 20:55:33 +0900

    chore: make ArtBoard 컴포넌트

---
+++ src/components/UI/ArtBoard.tsx
@@ -0,0 +1,5 @@
+export default function ArtBoard(): React.ReactNode {
+  return (
+    <div className="artboard phone-1 rounded-3xl border border-slate-100 p-5 shadow-md"></div>
+  );
+}
--- src/pages/FreeForAllFormPage.tsx
+++ src/pages/FreeForAllFormPage.tsx
@@ -1,11 +1,11 @@
+import ArtBoard from "../components/UI/ArtBoard";
 import Layout from "../components/UI/Layout";
 
 export default function FreeForAllFormPage(): React.ReactNode {
   return (
     <Layout>
       <div className="flex h-full w-full items-center justify-center gap-20">
-        {/* <div className="h-4/6 w-4/12 rounded-3xl border border-slate-100 p-5 shadow-md"></div> */}
-        <div className="artboard phone-1 rounded-3xl border border-slate-100 p-5 shadow-md"></div>
+        <ArtBoard />
       </div>
     </Layout>
   );
