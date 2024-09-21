b4341b712249cba17edf79d8ca942aced59bb32b
tyange <usun16@gmail.com>
2024-09-21 20:52:56 +0900

    chore: set the artboard

--- src/pages/FreeForAllFormPage.tsx
+++ src/pages/FreeForAllFormPage.tsx
@@ -3,8 +3,9 @@ import Layout from "../components/UI/Layout";
 export default function FreeForAllFormPage(): React.ReactNode {
   return (
     <Layout>
-      <div>
-        <p>Free For All Form</p>
+      <div className="flex h-full w-full items-center justify-center gap-20">
+        {/* <div className="h-4/6 w-4/12 rounded-3xl border border-slate-100 p-5 shadow-md"></div> */}
+        <div className="artboard phone-1 rounded-3xl border border-slate-100 p-5 shadow-md"></div>
       </div>
     </Layout>
   );
