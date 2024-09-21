b223cecf345811a262cbee50a9d3eb477ff60710
tyange <usun16@gmail.com>
2024-09-21 10:13:06 +0900

    add: free for all form page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import MainPage from "./pages/MainPage";
+import FreeForAllFormPage from "./pages/FreeForAllFormPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainPage />,
   },
+  { path: "/free-for-all-form", element: <FreeForAllFormPage /> },
 ]);
 
 export default function App(): React.ReactNode {
---
+++ src/pages/FreeForAllFormPage.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/UI/Layout";
+
+export default function FreeForAllFormPage(): React.ReactNode {
+  return (
+    <Layout>
+      <div>
+        <p>Free For All Form</p>
+      </div>
+    </Layout>
+  );
+}
